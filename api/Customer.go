bce6cdc61f2fc882a27cf0837f10f2325a19780c
briansimpson23 <briansimpson23@gmail.com>
2013-10-05 23:35:41 -0400

    Update and rename Member.go to Customer.go

---
+++ api/Customer.go
@@ -0,0 +1,10 @@
+package api
+
+type Customer struct {
+	CustomerID           int64
+	CompanyName string
+	FirstName          string
+	LastName           string
+	Email              string
+	ZipCode            string
+}
